2381c144416c709e65bf8c0770c5b9a9cf4f1072
Andrew Peacock <apeacock18@my.whitworth.edu>
2018-04-09 16:26:10 -0700

    Update create skill form

--- src/components/createSkill.js
+++ src/components/createSkill.js
@@ -5,6 +5,7 @@ class CreateSkill extends React.Component {
   constructor() {
     super();
     this.state = {
+      platform: "amazon",
       email: "",
       fName: "",
       lName: "",
@@ -77,121 +78,158 @@ class CreateSkill extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1 className="pageHeader">Create Skill</h1>
         <div className="newSkillForm">
           <form onSubmit={this.submitForm}>
-            <input
-              type="radio"
-              name="platform"
-              value="amazon"
-              checked="true"
-              onChange={this.handleInputChange}
-            />
-            {" Amazon Alexa"}
-            <br />
-            <input
-              type="radio"
-              name="platform"
-              value="google"
-              onChange={this.handleInputChange}
-            />
-            {" Google Voice"}
-            <br />
-
-            <h3 className="lblBig">First Name</h3>
-            <input
-              type="text"
-              name="fName"
-              placeholder="e.g. John"
-              onChange={this.handleInputChange}
-            />
-            <h3 className="lblBig">Last Name</h3>
-            <input
-              type="text"
-              name="lName"
-              placeholder="e.g. Smith"
-              onChange={this.handleInputChange}
-            />
-            <h3 className="lblBig">Verification Email</h3>
-            <input
-              type="text"
-              name="email"
-              placeholder="e.g. you@example.com"
-              onChange={this.handleInputChange}
-            />
-
-            <h3 className="lblBig">
-              What is the name you want to appear in the Amazon or Google store?
-            </h3>
-            <input
-              type="text"
-              name="skillName"
-              placeholder="e.g. Apple Color Finder"
-              onChange={this.handleInputChange}
-            />
-            <h6 className="lblSmall">MetaVoice Application Name</h6>
-            <h3 className="lblBig">
+            <div className="form-group">
+              {" "}
+              <label htmlFor="exampleInputEmail1">Email address</label>{" "}
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Email"
+              />{" "}
+            </div>
+            <div className="form-group">
+              <div className="radio">
+                <label className="form-check-label">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="platform"
+                    value="amazon"
+                    checked={this.state.platform === "amazon"}
+                    onChange={this.handleInputChange}
+                  />
+                  Amazon Alexa
+                </label>
+              </div>
+              <div className="radio">
+                <label className="form-check-label">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="platform"
+                    value="google"
+                    checked={this.state.platform === "google"}
+                    onChange={this.handleInputChange}
+                  />
+                  Google Voice
+                </label>
+              </div>
+            </div>
+            <div className="form-group">
+              <label>First Name</label>
+              <input
+                className="form-control"
+                type="text"
+                name="fName"
+                placeholder="e.g. John"
+                onChange={this.handleInputChange}
+              />
+              <label>Last Name</label>
+              <input
+                className="form-control"
+                type="text"
+                name="lName"
+                placeholder="e.g. Smith"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <label className="lblBig">Verification Email</label>
+              <input
+                className="form-control"
+                type="text"
+                name="email"
+                placeholder="e.g. you@example.com"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <label className="lblBig">
+                What is the name you want to appear in the Amazon or Google
+                store?
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="skillName"
+                placeholder="e.g. Apple Color Finder"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <label className="lblBig">
               What do you want people to say to invoke or start your MetaVoice
               application?
-            </h3>
+            </label>
             <input
+              className="form-control"
               type="text"
               name="invocation"
               placeholder="e.g. Apple Color Finder"
               onChange={this.handleInputChange}
             />
-            <h6 className="lblSmall">Invocation Name</h6>
-            <h3 className="lblBig">
+            <label className="lblBig">
               What should people ask or say to your voice application?
-            </h3>
+            </label>
             <input
+              className="form-control"
               type="text"
               name="utterance1"
               placeholder="e.g. What color are apples?"
               onChange={this.handleInputChange}
             />
             <input
+              className="form-control"
               type="text"
               name="utterance2"
               placeholder="e.g. Apples are what color?"
               onChange={this.handleInputChange}
             />
             <input
+              className="form-control"
               type="text"
               name="utterance3"
               placeholder="e.g. Apple colors"
               onChange={this.handleInputChange}
             />
             <input
+              className="form-control"
               type="text"
               name="utterance4"
               placeholder="e.g. Are apples red?"
               onChange={this.handleInputChange}
             />
             <input
+              className="form-control"
               type="text"
               name="utterance5"
               placeholder="e.g. Are apples green?"
               onChange={this.handleInputChange}
             />
-            <h6 className="lblSmall">Utterances</h6>
-            <h3 className="lblBig">
+            <label className="lblBig">
               What will your voice application say back?
-            </h3>
+            </label>
             <input
+              className="form-control"
               type="text"
               name="response"
               placeholder="e.g. Apples are red."
               onChange={this.handleInputChange}
             />
-            <h6 className="lblSmall">Responses</h6>
 
-            <h3 className="lblBig">
+            <label className="lblBig">
               What category will your MetaVoice application show up as on the
               store?
-            </h3>
-            <select name="category" onChange={this.handleInputChange}>
+            </label>
+            <select
+              className="form-control"
+              name="category"
+              onChange={this.handleInputChange}
+            >
               <option value="Business & Finance">Business and Finance</option>
               <option value="Connected Car">Connected Car</option>
               <option value="Education and Refference">
@@ -219,32 +257,37 @@ class CreateSkill extends React.Component {
               </option>
               <option value="Utilities">Utilities</option>
             </select>
-            <h3 className="lblBig">
+            <label className="lblBig">
               Enter descriptions of your MetaVoice application
-            </h3>
+            </label>
             <input
+              className="form-control"
               type="text"
               name="skillDescShort"
               placeholder="e.g. Enter a short description of your skill, the one liner"
               onChange={this.handleInputChange}
             />
             <input
+              className="form-control"
               type="text"
               name="skillDescLong"
               placeholder="e.g. Enter the full description of your skill"
               onChange={this.handleInputChange}
             />
-            <h3 className="lblBig">
+            <label className="lblBig">
               What keywords do you want your MetaVoice application to have?
-            </h3>
+            </label>
             <input
+              className="form-control"
               type="text"
               name="keywords"
               placeholder="e.g. Enter keywords for your skill separated by commas"
               onChange={this.handleInputChange}
             />
 
-            <button type="submit">Submit</button>
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
           </form>
         </div>
       </div>
