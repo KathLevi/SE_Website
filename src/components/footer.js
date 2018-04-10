13ee0827c593626519741002efd25c326693e5cb
eparslow18 <eparslow18@my.whitworth.edu>
2018-04-10 15:39:37 -0700

    rearranged navigation and footer components

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => (
-  <footer>
-    <div className="text-center">@2018 - Blue Marble. All rights reserved.</div>
+  <footer>  
+    <nav className="menu">
+    <ul className="fmenuList text-center">
+    @2018 - Blue Marble. All rights reserved.    
+      <li className="fmenuListElement">
+          <NavLink exact activeClassName="current" to="/team">
+            OUR TEAM
+          </NavLink>
+        </li>
+        <li className="fmenuListElement">
+        <NavLink exact activeClassName="current" to="/contact">
+          CONTACT US
+        </NavLink>
+      </li>
+        </ul>
+  </nav>
+    
   </footer>
 );
 
