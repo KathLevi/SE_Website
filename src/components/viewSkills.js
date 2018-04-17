88dd96a94325ad5a412478e11fe6ad3d2f214891
eparslow18 <eparslow18@my.whitworth.edu>
2018-04-16 21:13:40 -0700

    work on view skills page

--- src/components/viewSkills.js
+++ src/components/viewSkills.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { PageHeader } from "react-bootstrap";
 
 const ViewSkills = () => (
-  <div>
-    <h1 className="pageHeader">View Skills</h1>
+  <div className="container">
+    <PageHeader>My Skills
+      <NavLink
+        className="btn btn-primary pull-right"
+          exact
+          activeClassName="current"
+          to="/create-skill"
+              >
+          Create New Skill
+      </NavLink>
+    </PageHeader>
+      <table className="table"> 
+        <tr>
+          <th>SKILL NAME</th> 
+          <th>LANGUAGE</th>
+          <th>TYPE</th>
+          <th>MODIFIED</th>
+          <th>STATUS</th>
+          <th>ACTIONS</th>
+        </tr>
+      </table>
   </div>
 );
 
