fc1ea5efb241642938c1511d68c3329fdfff3497
eparslow18 <eparslow18@my.whitworth.edu>
2018-04-16 22:14:52 -0700

    View skills cont.

--- src/components/viewSkills.js
+++ src/components/viewSkills.js
@@ -23,6 +23,56 @@ const ViewSkills = () => (
           <th>STATUS</th>
           <th>ACTIONS</th>
         </tr>
+        <tr>
+          <td>Test 01</td>
+          <td>English (US)</td>
+          <td>Flash Briefing</td>
+          <td>04-16-2018</td>
+          <td>In Development</td>
+          <td>
+          <NavLink
+                className="btn"
+                exact
+                activeClassName="current"
+                to="/view-skills"
+              >
+                Edit
+              </NavLink> | 
+              <NavLink
+                className="btn"
+                exact
+                activeClassName="current"
+                to="/view-skills"
+              >
+                Delete
+              </NavLink>
+            </td>
+        </tr>
+        <tr>
+          <td>Test 02</td>
+          <td>English (UK)</td>
+          <td>Simple Skill</td>
+          <td>04-17-2018</td>
+          <td>In Certification</td>
+          <td>
+          <NavLink
+                className="btn"
+                exact
+                activeClassName="current"
+                to="/view-skills"
+              >
+                Edit
+              </NavLink> | 
+              <NavLink
+                className="btn"
+                exact
+                activeClassName="current"
+                to="/view-skills"
+              >
+                Delete
+              </NavLink>
+            </td>
+        </tr>
       </table>
   </div>
 );
