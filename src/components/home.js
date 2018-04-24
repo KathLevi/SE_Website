79fdbfe0ccc5646e89859f4afa629e6bcd322ebe
Kathleen Levi <levi.kathleen@gmail.com>
2018-04-24 15:21:41 -0700

    updated homepage and logo

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,24 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import home_logo from "../assets/home_logo.png";
+import { NavLink } from "react-router-dom";
 
 const Home = () => (
-  <div className="home-section-1">
-    <img src={logo} alt="blue marble logo" />
+  <div className="home-section">
+    <div className="home-section-2">
+      <h1>Blue <span>Marble</span></h1>
+      <h3>Create a new skill with one simple form.</h3>
+      <NavLink
+      className="btn btn-primary"
+      exact
+      activeClassName="current"
+      to="/demo">
+      LEARN MORE
+      </NavLink>
+    </div>
+  
+    <div className="home-section-1">
+      <img src={home_logo} alt="blue marble logo" />
+    </div>
   </div>
 );
 
