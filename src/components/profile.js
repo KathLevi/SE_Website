d7fbead76a93d019934b6c6f41c06e4deb41f5e7
Kathleen Levi <levi.kathleen@gmail.com>
2018-04-24 15:36:31 -0700

    fixed footer padding

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,7 +6,12 @@ class Profile extends React.Component {
   constructor() {
     super();
     this.state = {
-      userName : window.localStorage.getItem("userId")
+      userName : window.localStorage.getItem("userId"),
+      fName: "Hello",
+      lName: "World",
+      phoneNum: "123-456-7890",
+      address: "300 W Hawthorne Rd, Spokane WA 12345",
+      email: "test@test.com"
     };
   }
 
@@ -14,6 +19,16 @@ class Profile extends React.Component {
     return (
       <div className="container">
         <PageHeader>Hello, {this.state.userName}</PageHeader>
+        <div>USER INFO</div>
+        <div>Name: {this.state.fName} {this.state.lName}</div>
+        <div>Company: {this.state.address}</div>
+        <div>Email: {this.state.email}</div>
+
+        <div>CONTACT INFO</div>
+        <div>Phone Number: {this.state.phoneNum}</div>
+        <div>Address: {this.state.address}</div>
+        <div>Email: {this.state.email}</div>
+
       </div>  
 
     );
