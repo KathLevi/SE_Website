162c077b4bf080478caa367b4661ed8bb5550d1b
Kathleen Levi <levi.kathleen@gmail.com>
2018-04-26 15:30:05 -0700

    fixed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => (
   <footer>   
-    <div className = "footerCol text-left">
+    <div className = "footerCol text-left leftCol">
       <span>MORE BLUE MARBLE</span>
       <p className="fmenuListElement">
         <NavLink exact activeClassName="footer_current" to="/team">
@@ -21,7 +21,7 @@ const Footer = () => (
       <p>info@bluemarble.com</p>
       <p>(123) 456-7890</p>
     </div>
-    <div className = "footerCol text-right">
+    <div className = "footerCol text-right rightCol">
       <span>SOCIAL MEDIA</span>
       <p>insert social media icons here</p>
     </div> 
