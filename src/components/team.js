804b686f7d0f9adce7f658f84fc7292eef58f414
Kathleen Levi <levi.kathleen@gmail.com>
2018-04-29 23:07:02 -0700

    Changed meet our team header

--- src/components/team.js
+++ src/components/team.js
@@ -1,14 +1,9 @@
 import React from "react";
+import { PageHeader } from "react-bootstrap";
 
 const Team = () => (
-  <div>
-    <h1 className="pageHeader">
-      Meet our <span>Team</span>
-    </h1>
-    <h3 className="pageSubheader">
-      Click an image to learn more about one of our team members
-    </h3>
-    <div align="center" />
+  <div className="container">
+    <PageHeader>Meet Our Team</PageHeader>
   </div>
 );
 
