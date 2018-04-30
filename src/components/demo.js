b653021bfe9ea6b2d534ba399c8bff35bc7ccb58
Kathleen B Levi <klevi18@my.whitworth.edu>
2018-04-29 17:39:14 -0700

    fixed footer colors and deleted demo video placeholder

--- src/components/demo.js
+++ src/components/demo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import placeholder from "../assets/placeholder.mp4";
 import logo from "../assets/logo.png";
 
 const Demo = () => (
@@ -8,7 +7,7 @@ const Demo = () => (
 
     <div className="demoVid" align="center">
       <video width="800" height="500" controls poster={logo}>
-        <source src={placeholder} type="video/mp4" />
+        <source src="" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
